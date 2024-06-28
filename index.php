f544485ad853804b169d36d5afa39b4ffc8f2282
mahdisalary <149619452+mahdisalary@users.noreply.github.com>
2024-06-28 19:09:13 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body background="background%20web.jpg" >
+
+<table align="center" border="4" bordercolor="Black" width="700" height="1000">
+    <tr>
+        <td height="200" >
+            <img src= "back1.jpg" height="230" width="700" border="2" >
+
+
+
+        </td>
+    <tr>
+        <td border="4" bgcolor="#daa520" width="695" height="100" >
+            <marquee diraction="alternote" >
+              <img src="Untitled-1.png" width="690" height="90" m >
+            </marquee>
+
+
+
+        </td>
+    </tr>
+
+    <tr  border="4" bgcolor="#daa520" height="100" >
+        <td>
+            <table align="center"  cellpadding="5" cellspacing="5" >
+                <tr width="20" >
+
+                    <td height="60" > <a href="parvane-vekalat.psd"> <img border="2" src="مدارک1.png" width="70" height="55" ></a> </td>
+                    <td height="60" > <a href="اسم.docx"> <img border="2" src="موکلین%20سابق.png" width="70" height="55" > </td>
+                    <td height="60"> <img border="2" src="توضیحات.png" width="70" height="55" </td>
+                    <td height="60"> <img border="2" src="تماس%20با%20ما.png" width="70" height="55" </td>
+
+                </tr>
+
+
+            </table>
+        </td>
+    </tr>
+    <tr border="4" bgcolor="#daa520" >
+        <td align="center" >
+            <video  width="300" height="200" controls>
+                <source src="تبلیغ.mp4" type="video/mp4" >
+            </video>
+
+        </td>
+    </tr>
+    <tr>
+        <td bgcolor="#daa520" height="150">
+            <img border="1" src="کانون-وکلا-qbmbdgjl9jrcm1awuqxwmywhwr45r7cav7zjiu7kys.webp">
+            <img border="1" src="دادگستری-شیراز-qbmbdgjl9jrcm1awuqxwmywhwr45r7cav7zjiu7kys.webp">
+
+            <ul align="right" type="circle">
+                <li> آدرس:نیومکزیکو.آلبوکرکی.بلوار مونتگومری.میدان بهشت.دفتر سال گودمن </li>
+                <li> شماره تماس:505-842-5662</li>
+
+
+            </ul>
+
+        </td>
+
+    </tr>
+
+</table>
+
+
+</body>
+</html>
